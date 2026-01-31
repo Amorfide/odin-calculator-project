d4287b8795a6223d046a9a2a6b7e9ccebdf43137
amorfide <73080161+Amorfide@users.noreply.github.com>
2026-01-31 18:45:13 +0000

    changed event listener to the container, with logic to check button id

--- js-file.js
+++ js-file.js
@@ -1,8 +1,11 @@
 const btn = document.querySelectorAll(".number");
 const screen = document.querySelector(".screen");
+const btnContainer = document.getElementById("button-container")
 
-btn.forEach(btn => {
-    btn.addEventListener("click", (event) => {
+btnContainer.addEventListener("click", (event) => {
+    if(event.target.id === "number" || event.target.id === "operator"){
         screen.textContent += event.target.textContent;
-    })
+    }
+
 });
+
